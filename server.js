2c95d431b4cd43cb7fa7680a959b84607971d2ae
Ryan <shuttsie@gmail.com>
2020-09-24 18:05:48 -0500

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
 var Pusher = require('pusher');
 
 var pusher = new Pusher({
-    appId: '1079195',
-    key: '753fa0e07c31c514709e',
-    secret: 'bde7b5753f486cc750e7',
-    cluster: 'us2',
-    encrypted: true
+  appId: '1079290',
+  key: '346aa30b9201bc2509da',
+  secret: '0bcfe67ca7bd662dd9a0',
+  cluster: 'us2',
+  encrypted: true,
 });
 
 app.use(bodyParser.json());
@@ -18,23 +18,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.post('/message/send', (req, res) => {
-    pusher.trigger( 'private-chatapp', 'messages', {
-        message: req.body.message,
-        username: req.body.username
-    });
-    res.sendStatus(200);
+  pusher.trigger('private-chatapp', 'messages', {
+    message: req.body.message,
+    username: req.body.username,
+  });
+  res.sendStatus(200);
 });
 
 app.post('/pusher/auth', (req, res) => {
-    console.log('POST to /pusher/auth');
-    const socketId = req.body.socket_id;
-    const channel = req.body.channel_name;
-    const auth = pusher.authenticate(socketId, channel);
-    res.send(auth);
+  console.log('POST to /pusher/auth');
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-    domain: 'test-mang.us.auth0.com',
-    clientId: 'oedTrL34UTL0Ke6EzpImW1fe3JxMmv5X',
-    callbackUrl: 'https://master.d2vmu0oh0p7aj.amplifyapp.com' //Callback URL set in the Auth0 dasbhoard.
-}
+  domain: 'test-mang.us.auth0.com',
+  clientId: '3g6ud8oOEgq3WjViOzE3sXpHL4P8jArg',
+  callbackUrl: 'http://localhost:3000/callback', //Callback URL set in the Auth0 dasbhoard.
+};
