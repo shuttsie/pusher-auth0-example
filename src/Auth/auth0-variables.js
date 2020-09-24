c9ddbcf54fb8e338755a565315f1e9ba0f505641
rstrasser04 <69007424+rstrasser04@users.noreply.github.com>
2020-09-24 15:28:33 -0500

    Update auth0-variables.js

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     domain: 'test-mang.us.auth0.com',
     clientId: '3g6ud8oOEgq3WjViOzE3sXpHL4P8jArg',
-    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+    callbackUrl: 'https://master.d2vmu0oh0p7aj.amplifyapp.com/callback' //Callback URL set in the Auth0 dasbhoard.
 }
